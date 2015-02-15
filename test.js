0ca6d4b15f8de4282cf6f20f5626735172ef5f4d
Test <Test@email>
2015-02-15 12:20:43 +0300

    WoW

---
+++ test.js
@@ -0,0 +1,17 @@
+function test(err, stdout, stdrr) {
+  var show = stdout.trim()
+console.log("err: " + err)
+console.log("stdout: " + stdout)
+console.log("stdrr: " + stdrr)
+
+  if (show.match("nothing to commit")) {
+    console.log("Changes have been committed!")
+  }
+  else if (show.match("Changes not staged for commit")){
+    console.log("Seems there are changes\nto commit still.")
+  } else console.log("Hmm, can't find\ncommitted changes.")
+}
+
+var exec = require('child_process').exec
+exec('git status', test)
+console.log("hello");
